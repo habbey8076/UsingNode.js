f78af452162d81545655ab5110049194293b7151
habbey8076 <42881182+habbey8076@users.noreply.github.com>
2018-09-08 01:41:08 +0300

    New Commit

--- newApp.js
+++ newApp.js
@@ -3,13 +3,58 @@ console.log('Starting app.js');
 const fs = require('fs');
 const os = require('os');
 const _ = require ('lodash');
+const yargs = require ('yargs');
+
+
 const note = require('./note.js');
+
+
+const argv = yargs.argv;
+var command = argv._[0];
+
+console.log('Command:', command);
+//console.log('Process',process.argv);
+
+console.log('Yargs',argv);
+
+if (command == 'add'){
+   // console.log('Adding new note');
+    note.addNote(argv.title, argv.body);
+
+
+}else if (command == 'list'){
+    //console.log('Listing all notes');
+    note.getAll();
+
+}else if (command == 'read'){
+    //console.log('Read all notes');
+    note.readAll(argv.title);
+
+
+}else if (command == 'remove'){
+
+    note.removeAll();
+   // console.log('Remove all notes');
+}
+else{
+    console.log('comand not recongnize');
+}
+
+
+
+
+
+
+
+
+
 
 //console.log(_.isString(true));
 //console.log(_.isString('Abiodun'));
 
-var filteredArray = _.uniq(['Abiodun', 1, 'Abiodun', 1, 2,3,4]);
-console.log(filteredArray);
+//var filteredArray = _.uniq(['Abiodun', 1, 'Abiodun', 1, 2,3,4]);
+//var filteredArray = _.uniq(['Abiodun']);
+//console.log(filteredArray);
 
 
 
--- note.js
+++ note.js
@@ -1,14 +1,50 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = function ()  {
-  console.log('addNote');
-  return 'New note';
 
+var addNote = (title, body) =>{
+console.log('Adding note', title, body);
+
+};
+
+var getAll = (title, body) => {
+  console.log('Getting all notes');
+};
+
+var readAll = (title) => {
+
+  console.log('Getting note', title);
+};
+
+var removeAll = (title) =>{
+
+  console.log('Remove all Notes', title);
+};
+module.exports = {
+
+  addNote,
+  getAll,
+  readAll,
+  removeAll
 
 };
 
-module.exports.add =  (a, b) => {
 
-  return a + b;
 
-};
+
+
+
+
+
+
+//module.exports.addNote = function ()  {
+ // console.log('addNote');
+  //return 'New note';
+
+
+//};
+
+//module.exports.add =  (a, b) => {
+
+  //return a + b;
+
+//};
